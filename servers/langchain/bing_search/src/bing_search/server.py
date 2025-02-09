dda5dfd7d12090d36f3b4ded98fea26df13fd993
Gionatan Cernusco <gionatan99@gmail.com>
2025-02-09 11:45:47 +0100

    chore: adds e2b.toml

--- servers/langchain/bing_search/src/bing_search/server.py
+++ servers/langchain/bing_search/src/bing_search/server.py
@@ -62,7 +62,7 @@ async def main():
             write_stream,
             InitializationOptions(
                 server_name="bing_search",
-                server_version="1.36.0",
+                server_version="0.1.0",
                 capabilities=server.get_capabilities(
                     notification_options=NotificationOptions(),
                     experimental_capabilities={},
